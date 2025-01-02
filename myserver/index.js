08cba9a187777b3f045cce60e7794bddb73846f4
DeepeshPandey63 <deepesh639394@gmail.com>
2025-01-02 12:57:59 +0530

    set up of mongodb user model almost completed

--- myserver/index.js
+++ myserver/index.js
@@ -1,1 +1,43 @@
-const express=require("express")
+const express=require("express");
+const mongoose=require('mongoose');
+const User=require('./models/user');
+
+const app=express();
+const PORT=3800;
+const MongoURL="mongodb://127.0.0.1:27017/TaskingUser";
+app.use(express.json());
+
+mongoose.connect(MongoURL).then(()=>{console.log("mongodb connected")}).catch((err)=>{console.log(err)});
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+
+
+app.post('/signin', async (req, res) => { 
+    try { 
+        const user = new User(req.body);
+        await user.save(); 
+        res.status(201).send(user); 
+    } catch (error) { 
+        res.status(400).send(error); 
+    } 
+});
+
+
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server started at port: ${PORT}`);
+})
+
+
+
+
+
+
---
+++ myserver/models/user.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:"string",
+        required:true
+    },
+    emailId:
+    {
+        type:"string",
+        required:true,
+        unique:true
+    },
+    password:
+    {
+        type:"string",
+        required:true
+    },
+    gender: { 
+        type: String, enum: ['Male', 'Female', 'Other'],
+        required: true 
+    }
+
+}); 
+const User = mongoose.model('User', userSchema); 
+module.exports = User;
