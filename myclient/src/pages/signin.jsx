8e25b0ea80823a10e82567dd25e59e7b592eb174
khantnandeesh <148197396+khantnandeesh@users.noreply.github.com>
2025-01-03 19:03:58 +0530

    Update signin.jsx adding /signin/signin  when using fetch

--- myclient/src/pages/signin.jsx
+++ myclient/src/pages/signin.jsx
@@ -17,7 +17,7 @@ const Signinpage = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        fetch('http://localhost:3800/signin', {
+        fetch('http://localhost:3800/signin/signin', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
